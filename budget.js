521f65d408cc4ab0389218cdcea9b173e8f5ffa7
Keshav Kumar <71294563+keshav-fixed-404@users.noreply.github.com>
2022-05-02 02:58:03 +0530

    Update budget.js

--- budget.js
+++ budget.js
@@ -1,5 +1,3 @@
-/* www.youtube.com/CodeExplained */
-
 // SELECT ELEMENTS
 const balanceEl = document.querySelector(".balance .value");
 const incomeTotalEl = document.querySelector(".income-total");
